8899363aed3208df010e9c9870ee9ff5af00efe3
wallenas <peter.wallenas@gmail.com>
2021-08-24 09:22:07 +0200

    Minor sizing fixes

--- bish-bosh.js
+++ bish-bosh.js
@@ -5,44 +5,54 @@ function rangeUpdater(label, rangeElement) {
 
 
 function bishBosh() {
+    // Clear items
     document.getElementById('numberList').innerHTML = "";
+
     var amount = document.getElementById('count').value;
     var bishTal = document.getElementById('bish').value;
     var boshTal = document.getElementById('bosh').value;
 
     for (let i = 1; i <= amount; i++) {
+       /*
         console.log(`Loop i: ${i}
                     \nBish = ${bishTal} 
                     \nBosh = ${boshTal}`);
 
-        var pElement = document.createElement("li");
+        */
+
+        // Creates listitem
+        var liElement = document.createElement("li");
         var outDiv = document.getElementById("numberList");
         
         if (i % bishTal == 0 && i % boshTal == 0) {
-            console.log("bish-bosh")
-            pElement.innerHTML = 'Bish-Bosh';
-            pElement.style.color = "green";
-            outDiv.appendChild(pElement);
+            // console.log("bish-bosh")
+            
+            liElement.innerHTML = 'Bish-Bosh';
+            liElement.style.color = "green";
+            outDiv.appendChild(liElement);
         }
 
         else if (i % bishTal === 0) {
-            console.log("bish")
-            pElement.innerHTML = 'Bish';
-            pElement.style.color = "red"
-            outDiv.appendChild(pElement);
+            // console.log("bish")
+
+            liElement.innerHTML = 'Bish';
+            liElement.style.color = "red"
+            outDiv.appendChild(liElement);
         }
 
         else if (i % boshTal == 0){
-            console.log("bosh")
-            pElement.innerHTML = "Bosh";
-            pElement.style.color = "lightblue"
-            outDiv.appendChild(pElement);
+            // console.log("bosh")
+
+            liElement.innerHTML = "Bosh";
+            liElement.style.color = "lightblue"
+            outDiv.appendChild(liElement);
         }
 
         else {
-            console.log("index")
-            pElement.innerHTML = i;
-            outDiv.appendChild(pElement);
+            // console.log("index")
+            
+            liElement.innerHTML = i;
+            outDiv.appendChild(liElement);
         }
 
         
