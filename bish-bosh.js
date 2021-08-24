e52117d0f9b74e7226c1e56490a03fa58c0442eb
wallenas <peter.wallenas@gmail.com>
2021-08-24 09:05:29 +0200

    Basic style implemented - Made it so that all ranges control the items, not just "antal"

--- bish-bosh.js
+++ bish-bosh.js
@@ -5,37 +5,43 @@ function rangeUpdater(label, rangeElement) {
 
 
 function bishBosh() {
-    document.getElementById('hej').innerHTML = "";
+    document.getElementById('numberList').innerHTML = "";
     var amount = document.getElementById('count').value;
     var bishTal = document.getElementById('bish').value;
     var boshTal = document.getElementById('bosh').value;
 
     for (let i = 1; i <= amount; i++) {
-        console.log(`Loop i: ${i}\nBish = ${bishTal} \nBosh = ${boshTal}`);
-        var pElement = document.createElement("label");
-        var outDiv = document.getElementById("hej");
+        console.log(`Loop i: ${i}
+                    \nBish = ${bishTal} 
+                    \nBosh = ${boshTal}`);
+
+        var pElement = document.createElement("li");
+        var outDiv = document.getElementById("numberList");
         
         if (i % bishTal == 0 && i % boshTal == 0) {
             console.log("bish-bosh")
-            pElement.innerHTML = '  Bish-Bosh  ';
+            pElement.innerHTML = 'Bish-Bosh';
+            pElement.style.color = "green";
             outDiv.appendChild(pElement);
         }
 
         else if (i % bishTal === 0) {
             console.log("bish")
-            pElement.innerHTML = '  Bish  ';
+            pElement.innerHTML = 'Bish';
+            pElement.style.color = "red"
             outDiv.appendChild(pElement);
         }
 
         else if (i % boshTal == 0){
             console.log("bosh")
-            pElement.innerHTML = "  Bosh  ";
+            pElement.innerHTML = "Bosh";
+            pElement.style.color = "lightblue"
             outDiv.appendChild(pElement);
         }
 
         else {
             console.log("index")
-            pElement.innerHTML = "  " + i + "  ";
+            pElement.innerHTML = i;
             outDiv.appendChild(pElement);
         }
 
