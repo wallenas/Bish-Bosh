9711f16452c8d936263f7b1f231babad445cb407
wallenas <peter.wallenas@gmail.com>
2021-08-25 10:55:32 +0200

    Added counters - General code-clean up

--- bish-bosh.js
+++ bish-bosh.js
@@ -1,61 +1,87 @@
-// Update range-labels
+/**
+ * Update range-labels with range value  
+ */ 
 function rangeUpdater(label, rangeElement) { 
     document.getElementById(label).innerHTML = document.getElementById(rangeElement).value; 
 }
 
-
+/**
+ * Adds li-elements to ul-list 'bishBoshList'.
+ */
 function bishBosh() {
-    // Clear items
-    document.getElementById('numberList').innerHTML = "";
-
+    // Ul-list
+    var listParent = document.getElementById('bishBoshList');
+    
+    // Range-values
     var amount = document.getElementById('count').value;
-    var bishTal = document.getElementById('bish').value;
-    var boshTal = document.getElementById('bosh').value;
+    var bishNumber = document.getElementById('bish').value;
+    var boshNumber = document.getElementById('bosh').value;
+    
+    // Counter-elements
+    var bishBoshCount = document.getElementById('bishbosh-count');
+    var bishCount = document.getElementById('bish-count');
+    var boshCount = document.getElementById('bosh-count');
+    
+    // Counters
+    var bishboshCounter = 0;
+    var bishCounter = 0
+    var boshCounter = 0;
 
+    // Clear items
+    listParent.innerHTML = '';
+    bishBoshCount.innerHTML, bishCount.innerHTML, boshCount.innerHTML = 0;
+
+    // Main loop
     for (let i = 1; i <= amount; i++) {
-       /*
-        console.log(`Loop i: ${i}
-                    \nBish = ${bishTal} 
-                    \nBosh = ${boshTal}`);
-
-        */
+        // console.log(`Loop i: ${i}`);
 
         // Creates listitem
-        var liElement = document.createElement("li");
-        var outDiv = document.getElementById("numberList");
+        var liElement = document.createElement('li');
         
-        if (i % bishTal == 0 && i % boshTal == 0) {
+        // Number equally divided with both Bish and Bosh
+        if (i % bishNumber == 0 && i % boshNumber == 0) {
             // console.log("bish-bosh")
             
+            // Set element text and color
             liElement.innerHTML = 'Bish-Bosh';
-            liElement.style.color = "green";
-            outDiv.appendChild(liElement);
+            liElement.className = 'green'; // Seems safer than liElement.style.color = 'green'
+            listParent.appendChild(liElement);
+
+            // Adds to counter
+            bishBoshCount.innerHTML = ++bishboshCounter;
+
         }
 
-        else if (i % bishTal == 0) {
-            // console.log("bish")
+        // Number equally divided by Bish
+        else if (i % bishNumber == 0) {
+            // console.log('Bish')
 
             liElement.innerHTML = 'Bish';
-            liElement.style.color = "red"
-            outDiv.appendChild(liElement);
+            liElement.className = 'red';
+            listParent.appendChild(liElement);
+
+            bishCount.innerHTML = ++bishCounter;
         }
 
-        else if (i % boshTal == 0){
-            // console.log("bosh")
+        // Number equally divided by Bosh
+        else if (i % boshNumber == 0){
+            // console.log('Bosh')
 
-            liElement.innerHTML = "Bosh";
-            liElement.style.color = "lightblue"
-            outDiv.appendChild(liElement);
+            liElement.innerHTML = 'Bosh';
+            liElement.className = 'blue';
+            listParent.appendChild(liElement);
+
+            boshCount.innerHTML = ++boshCounter;
         }
 
+        // Number not equally divided with bish nor bosh
         else {
-            // console.log("index")
+            // console.log('Number')
             
             liElement.innerHTML = i;
-            outDiv.appendChild(liElement);
+            listParent.appendChild(liElement);
         }
 
-        
     }
 
 
